84e9651943c01cf059ff82a5aa0f3726e0b751e9
PChunark <puttipongchunark@gmail.com>
2023-10-02 14:28:14 +0700

    finish cal Q3/2566

--- code/profilecalOtherRE.R
+++ code/profilecalOtherRE.R
@@ -4,8 +4,8 @@ data<-
   genProWind %>% 
   pivot_longer(-measurementname, names_to = "time", values_to = "gen_mw") %>%
   pivot_wider(names_from = "measurementname", values_from = "gen_mw") %>% 
-  mutate(total = select(., !c(time)) %>% rowSums(., na.rm = T)) %>% 
-  select(time, total)
+  mutate(wind = select(., !c(time)) %>% rowSums(., na.rm = T)) %>% 
+  select(time, wind)
 
 otherREProfileAtMaxHydro <- c(otherREProfileAtMaxHydro, list("genProWind" = data))
 # 02 Solar ####
@@ -13,8 +13,8 @@ data<-
   genProSolar %>% 
   pivot_longer(-measurementname, names_to = "time", values_to = "gen_mw") %>%
   pivot_wider(names_from = "measurementname", values_from = "gen_mw") %>% 
-  mutate(total = select(., !c(time)) %>% rowSums(., na.rm = T)) %>% 
-  select(time, total)
+  mutate(solar = select(., !c(time)) %>% rowSums(., na.rm = T)) %>% 
+  select(time, solar)
 
 otherREProfileAtMaxHydro <- c(otherREProfileAtMaxHydro, list("genProSolar" = data))
 
@@ -24,8 +24,8 @@ data<-
   genProWaste %>% 
   pivot_longer(-measurementname, names_to = "time", values_to = "gen_mw") %>%
   pivot_wider(names_from = "measurementname", values_from = "gen_mw") %>% 
-  mutate(total = select(., !c(time)) %>% rowSums(., na.rm = T)) %>% 
-  select(time, total)
+  mutate(waste = select(., !c(time)) %>% rowSums(., na.rm = T)) %>% 
+  select(time, waste)
 
 otherREProfileAtMaxHydro <- c(otherREProfileAtMaxHydro, list("genProWaste" = data))
 
@@ -34,8 +34,8 @@ data<-
   genProBiomass %>% 
   pivot_longer(-measurementname, names_to = "time", values_to = "gen_mw") %>%
   pivot_wider(names_from = "measurementname", values_from = "gen_mw") %>% 
-  mutate(total = select(., !c(time)) %>% rowSums(., na.rm = T)) %>% 
-  select(time, total)
+  mutate(biomass = select(., !c(time)) %>% rowSums(., na.rm = T)) %>% 
+  select(time, biomass)
 
 otherREProfileAtMaxHydro <- c(otherREProfileAtMaxHydro, list("genProBiomass" = data))
 
@@ -44,8 +44,8 @@ data<-
   genProSppNonFirmWasteGas %>% 
   pivot_longer(-measurementname, names_to = "time", values_to = "gen_mw") %>%
   pivot_wider(names_from = "measurementname", values_from = "gen_mw") %>% 
-  mutate(total = select(., !c(time)) %>% rowSums(., na.rm = T)) %>% 
-  select(time, total)
+  mutate(wastegas = select(., !c(time)) %>% rowSums(., na.rm = T)) %>% 
+  select(time, wastegas)
 
 otherREProfileAtMaxHydro <- c(otherREProfileAtMaxHydro, list("genProSppNonFirmWasteGas" = data))
 
@@ -54,8 +54,8 @@ data<-
   genProMiniHydro %>% 
   pivot_longer(-measurementname, names_to = "time", values_to = "gen_mw") %>%
   pivot_wider(names_from = "measurementname", values_from = "gen_mw") %>% 
-  mutate(total = select(., !c(time)) %>% rowSums(., na.rm = T)) %>% 
-  select(time, total)
+  mutate(minihydro = select(., !c(time)) %>% rowSums(., na.rm = T)) %>% 
+  select(time, minihydro)
 
 otherREProfileAtMaxHydro <- c(otherREProfileAtMaxHydro, list("genProMiniHydro" = data))
 
@@ -64,15 +64,15 @@ data<-
   genProBlackLiq %>% 
   pivot_longer(-measurementname, names_to = "time", values_to = "gen_mw") %>%
   pivot_wider(names_from = "measurementname", values_from = "gen_mw") %>% 
-  mutate(total = select(., !c(time)) %>% rowSums(., na.rm = T)) %>% 
-  select(time, total)
+  mutate(blackliquor = select(., !c(time)) %>% rowSums(., na.rm = T)) %>% 
+  select(time, blackliquor)
 
 otherREProfileAtMaxHydro <- c(otherREProfileAtMaxHydro, list("genProBlackLiq" = data))
 
 ls(otherREProfileAtMaxHydro)
 
-# genProREAtMaxHydro <-
+genProREAtMaxHydro <-
   otherREProfileAtMaxHydro %>%   
-  purrr::reduce(join_all, by = c(colnames(.)))
-  
-  Reduce(function(...) merge(..., all=T), otherREProfileAtMaxHydro)
+  purrr::reduce(full_join, by = "time") 
+
+writexl::write_xlsx(genProREAtMaxHydro, path = "processdata/genProREAtMaxHydro.xlsx")
