b4465d267f911c64d1263617a1e546e6de84ae7d
PChunark <puttipongchunark@gmail.com>
2023-10-20 16:05:55 +0700

    do for loop

--- code/try.R
+++ code/try.R
@@ -1,23 +1,61 @@
 i1 <- list()
-url <- list()
-file <- list()
+loadProfile <- list()
+max <- list() 
+year <- 2023
+
 for(i in 1:87) {
   i1[[i]] <- sprintf('%02d', i)
   url[[i]] <- paste0("http://example.com/orResultsED.cfm?MODE=exED&ED=", i1, "&EventId=31")
   file[[i]] <- paste0("Data/myExcel_", i, ".xlsx")
   # if (!file.exists(file)) download.file(url, file) 
 }
-year <- 2023
+# scraping from web
+
 for(i in 1:2) {
-  i1 <- sprintf('%02d', i)
+  i1[[i]] <- sprintf('%02d', i)
   url <- paste0("http://control.egat.co.th/GetNetGen/default.aspx?d=",i1,"/",year)
   xpath <- "/html/body/form"
-  loadProfile <-
-    url %>% 
+  loadProfile[[i]] <-
+    session(url[i]) %>% 
     read_html() %>% 
     html_element(xpath = xpath) %>% 
     html_table() %>% 
     rename_all(tolower)
-  # file <- paste0("Data/myExcel_", i, ".xlsx")
-  # if (!file.exists(file)) download.file(url, file) 
+}
+# Finding max value
+for (j in 1:ncol(loadProfile[[1]])){
+  
+  if(j>=ncol(loadProfile[[1]]))
+  {break}
+  else{
+    max[[j]] <- loadProfile[[i]] %>% 
+      select(timestamp, j+1) 
+  }
+  max[[j]] <- max[[j]] %>% 
+    filter(max[[j]][,2] == max(.[,2]))
+}
+#### Merge for loop
+
+for(i in 1:2) {
+  i1[[i]] <- sprintf('%02d', i)
+  url <- paste0("http://control.egat.co.th/GetNetGen/default.aspx?d=",i1,"/",year)
+  xpath <- "/html/body/form"
+  loadProfile[[i]] <-
+    session(url[i]) %>% 
+    read_html() %>% 
+    html_element(xpath = xpath) %>% 
+    html_table() %>% 
+    rename_all(tolower)
+  
+  for (j in 1:ncol(loadProfile[[i]])){
+    
+    if(j>=ncol(loadProfile[[i]]))
+    {break}
+    else{
+      max[[j]] <- loadProfile[[i]] %>% 
+        select(timestamp, j+1) 
+    }
+    max[[j]] <- max[[j]] %>% 
+      filter(max[[j]][,2] == max(.[,2]))
+  }  
 }
